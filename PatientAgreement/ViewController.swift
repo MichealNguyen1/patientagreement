2b56b9238f0393f63b4c8532aea2ab135ff0872b
Hieu Trinh Tien <hieu.trinh@pycogroup.com>
2018-03-30 13:59:38 +0700

    Init Project

--- PatientAgreement/ViewController.swift
+++ PatientAgreement/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var agreementPageView: UIView!
+    @IBOutlet weak var phonePageView: UIView!
+    @IBOutlet weak var surveyPageView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func titleSegmentChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            agreementPageView.isHidden = false
+            phonePageView.isHidden = true
+            surveyPageView.isHidden = true
+        case 1:
+            phonePageView.isHidden = false
+            agreementPageView.isHidden = true
+            surveyPageView.isHidden = true
+        case 2:
+            surveyPageView.isHidden = false
+            agreementPageView.isHidden = true
+            phonePageView.isHidden = true
+        default:
+            break
+        }
     }
-
-
+    
 }
 
